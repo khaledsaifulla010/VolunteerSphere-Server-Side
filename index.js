e3c5a0a04cf4ea2b187ae2ce1a743bc606df2a71
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-22 20:27:15 +0600

    connect with mongodb and secure it

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // Middlewares //
 
 app.use(cors());
 app.use(express.json());
+
+
+// Connect With MongoDB //
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ugbxhsw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
 
 // Server Running //
 
